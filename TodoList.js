e6a3cc5e6f7ee4ef2b118836f3578aad1e257b9e
Artem <107483805+Artem-dev2022@users.noreply.github.com>
2023-05-17 15:42:46 +0300

    add files

---
+++ TodoList.js
@@ -0,0 +1,98 @@
+import { TodoItem } from './TodoItem.js'
+
+export class TodoList {
+    _list = []
+    _listLength = 0
+    _listDoneLength = 0
+    _maxId = 0
+
+    constructor(container, amountCounter, amountDoneCounter, clearBtn) {
+        this.container = container
+        this.list = document.createElement('div')
+        this.list.classList.add('app__list')
+        this.amountCounter = amountCounter
+        this.amountDoneCounter = amountDoneCounter
+        this.clearBtn = clearBtn
+
+        container.innerHTML = ''
+        container.append(this.list)
+
+        this.checkEmpty()
+        this.getListLength()
+        this.getLastDoneLength()
+    }
+
+    getListLength(){
+        this.amountCounter.textContent = this._list.length
+        return this._list.length
+    }
+
+    getLastDoneLength(){
+        this._listDoneLength = this._list.filter(item => item.done == true).length
+        this.amountDoneCounter.textContent = this._list.length - this._listDoneLength
+    }
+
+    checkEmpty(){
+        if (!this._list.length) {
+            this.empty = document.createElement('div')
+            this.empty.classList.add('app__empty')
+            this.emptyText = document.createElement('p')
+            this.emptyText.classList.add('app__empty__text')
+            this.emptyText.textContent = 'to-do list is empty'
+
+            this.empty.append(this.emptyText)
+            this.list.append(this.empty)
+
+            this.clearBtn.classList.add('disabled')
+        } else {
+            this.empty ? [this.empty.remove(), this.clearBtn.classList.remove('disabled')] : null
+        }
+    }
+
+    getNewId(){
+        for (const item of this._list) {
+            if (item.id > this._maxId) this._maxId = item.id
+        }
+        this._maxId++
+        return this._maxId
+    }
+
+    add(title, done = false){
+        let id = this.getNewId()
+        let newTodoItem = new TodoItem(this, title, done, id)
+        this._list.push(newTodoItem)
+
+        this.checkEmpty()
+        this.getListLength()
+        this.getLastDoneLength()
+    }
+
+    delete(value) {
+        let id = value
+
+        if (value instanceof TodoItem){
+            id = value.id
+        }
+
+        for (let i = 0; i <this._list.length; i++) {
+            if (this._list[i].id == id) {
+                this._list.splice(i, 1)
+            }
+        }
+
+        this.checkEmpty()
+        this.getListLength()
+        this.getLastDoneLength()
+    }
+
+    clearList() {
+        this._list = []
+        this.list.innerHTML = ''
+
+        this.getListLength()
+        this.getLastDoneLength()
+        this.checkEmpty()
+    }
+    
+}
+
---
+++ script.js
@@ -0,0 +1,44 @@
+import { TodoItem } from './TodoItem.js'
+import { TodoList } from './TodoList.js'
+
+const container = document.getElementById('app-list')
+const listContainer = document.getElementById('list-container')
+const inputTodo = document.getElementById('input-todo')
+const addTodo = document.getElementById('add-todo')
+const errorMessage = document.querySelector('.error')
+const itemsAmount = document.getElementById('amount')
+const itemsDoneAmount = document.getElementById('amount-done')
+
+const clearPopup = document.querySelector('.dark-bg-clear')
+const clearBtn = document.querySelector('.app__bottom__btn')
+const clearDenyBtn = document.querySelector('.popup-clear__btn-deny')
+const clearClearBtn = document.querySelector('.popup-clear__btn-remove')
+
+
+let newList = new TodoList(listContainer, itemsAmount, itemsDoneAmount, clearBtn)
+
+addTodo.addEventListener('click', () => {
+    if (!inputTodo.value.length) {
+        errorMessage.classList.add('error-active')
+        return
+    } else {
+        errorMessage.classList.remove('error-active')
+    }
+
+    newList.add(inputTodo.value)
+
+    console.log(newList)
+    inputTodo.value = ''
+})
+
+clearBtn.addEventListener('click', () => {
+    clearPopup.classList.add('dark-bg-active')
+    clearDenyBtn.addEventListener('click', () => {
+        clearPopup.classList.remove('dark-bg-active')
+    })
+
+    clearClearBtn.addEventListener('click', () => {
+        newList.clearList()
+        clearPopup.classList.remove('dark-bg-active')
+    })
+})
